1e7cccddc54b9fe8453f78c1b0aaeb2e97b32418
Mathieu <mathieu.leothaud@etu.u-bordeaux.fr>
2017-11-08 21:17:45 +0100

    Ecriture head index

--- src/php/index.php
+++ src/php/index.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html> 
+<html lang="fr">
+    
+    <head>
+        <meta charset="utf-8">
+        <title>Share Your Time</title>
+        
+        <link href="./../css/style.css" rel="stylesheet">
+    </head>
+    
+    
+</html>
+
+
 <?php
 
 /* 
