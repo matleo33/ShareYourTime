80decf8244074d23cb3db04ce71b7a93547e148d
Mathieu <mathieu.leothaud@etu.u-bordeaux.fr>
2017-11-08 21:27:08 +0100

    nettoyage

--- src/php/index.php
+++ src/php/index.php
@@ -1,22 +1,38 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html lang="fr">
-    
-    <head>
-        <meta charset="utf-8">
-        <title>Share Your Time</title>
-        
-        <link href="./../css/style.css" rel="stylesheet">
-    </head>
-    
-    
+
+<head>
+    <meta charset="UTF-8">
+    <link href="./../css/style.css" rel="stylesheet">
+
+    <!-- Bootstrap-->
+    <link rel="stylesheet" href="./../BootStrap/css/bootstrap.min.css">
+
+
+    <title>Save Your Time</title>
+</head>
+
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Barre de navigation déroulante</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Save Your Time</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.php">Accueil</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<script src="./BootStrap/js/bootstrap.min.js"></script>
+<script src="./Jquery/jquery-3.2.1.min.js"></script>
+</body>
+
 </html>
-
-
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
