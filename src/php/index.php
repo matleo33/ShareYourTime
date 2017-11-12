b6eb231a94e818a44653d89038cdbd680eca546a
Mathieu <mathieu.leothaud@etu.u-bordeaux.fr>
2017-11-12 12:31:26 +0100

    Et la BD fût

--- src/php/index.php
+++ src/php/index.php
@@ -16,6 +16,30 @@
 <?php include 'navbar.include.php'; ?><br/>
 <?php include 'carousel.include.php'; ?>
 <?php include 'top_3_evenement.include.php'; ?>
+<?php
+try
+{
+$bdd = new PDO('mysql:host=localhost;dbname=shareyourtime;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+$reponse = $bdd->query('SELECT * FROM users');
+while ($donnees = $reponse->fetch())
+
+{
+
+?>
+    <p>
+    <strong>Prenom</strong> : <?php echo $donnees['prenom']; ?><br />
+    <strong>Nom</strong> :  <?php echo $donnees['nom']; ?>
+    </p>
+<?php
+}
+$reponse->closeCursor(); // Termine le traitement de la requête
+?>
+
 <script src="../BootStrap/js/bootstrap.min.js"></script>
 <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/fr/"><img alt="Licence Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/3.0/fr/88x31.png" /></a><br />Ce(tte) œuvre est mise à disposition selon les termes de la <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/fr/">Licence Creative Commons Attribution - Pas d’Utilisation Commerciale - Pas de Modification 3.0 France</a>.
 </body>
--- src/php/inscription.php
+++ src/php/inscription.php
@@ -1,8 +1,10 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+try
+{
+$bdd = new PDO('mysql:host=localhost;dbname=shareyourtime;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
 ?>
