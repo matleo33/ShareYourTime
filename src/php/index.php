6571019dfdd6674f7b1b71e535c0d1652ddaf2be
Mathieu <mathieu.leothaud@etu.u-bordeaux.fr>
2017-11-12 11:55:58 +0100

    modif licence

--- src/php/index.php
+++ src/php/index.php
@@ -17,6 +17,7 @@
 <?php include 'carousel.include.php'; ?>
 <?php include 'top_3_evenement.include.php'; ?>
 <script src="../BootStrap/js/bootstrap.min.js"></script>
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/fr/"><img alt="Licence Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/3.0/fr/88x31.png" /></a><br />Ce(tte) œuvre est mise à disposition selon les termes de la <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/fr/">Licence Creative Commons Attribution - Pas d’Utilisation Commerciale - Pas de Modification 3.0 France</a>.
 </body>
 
 
