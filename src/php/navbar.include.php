ee8cc070a9e6e76d36e83c5d5becba3a66a82d6d
Amaury <amaury.deguilhem@gmail.com>
2017-11-13 15:19:15 +0100

    Travail

--- src/php/navbar.include.php
+++ src/php/navbar.include.php
@@ -3,7 +3,7 @@ define('pagencours', $_SERVER['PHP_SELF'], true);
 $page = explode("/", pagencours);
 ?>
 
-
+<link href="./../css/style.css" rel="stylesheet">
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
@@ -28,6 +28,55 @@ $page = explode("/", pagencours);
                 <li class="nav-item <?php if (end($page) == "creer_evenement.php") { echo 'active'; } ?>">
                     <a href="<?php if (end($page) == "creer_evenement.php") { echo '#'; } else { echo 'creer_evenement.php'; } ?>">Créer événement</a></li>
             </ul>
+            <style>
+                .ui-autocomplete {
+                    position: absolute;
+                    top: 100%;
+                    left: 0;
+                    z-index: 1000;
+                    float: left;
+                    display: none;
+                    min-width: 160px;
+                    _width: 160px;
+                    padding: 4px 0;
+                    margin: 2px 0 0 0;
+                    list-style: none;
+                    background-color: #ffffff;
+                    border-color: #ccc;
+                    border-color: rgba(0, 0, 0, 0.2);
+                    border-style: solid;
+                    border-width: 1px;
+                    -webkit-border-radius: 5px;
+                    -moz-border-radius: 5px;
+                    border-radius: 5px;
+                    -webkit-box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+                    -moz-box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+                    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+                    -webkit-background-clip: padding-box;
+                    -moz-background-clip: padding;
+                    background-clip: padding-box;
+                    *border-right-width: 2px;
+                    *border-bottom-width: 2px;
+                }
+                .ui-autocomplete .ui-menu-item > a.ui-corner-all {
+                    display: block;
+                    padding: 3px 15px;
+                    clear: both;
+                    font-weight: normal;
+                    line-height: 18px;
+                    color: #555555;
+                    white-space: nowrap;
+                }
+                .ui-autocomplete .ui-menu-item > a.ui-corner-all.ui-state-hover, .ui-autocomplete .ui-menu-item > a.ui-corner-all.ui-state-active {
+                    color: #ffffff;
+                    text-decoration: none;
+                    background-color: #0088cc;
+                    border-radius: 0px;
+                    -webkit-border-radius: 0px;
+                    -moz-border-radius: 0px;
+                    background-image: none;
+                }
+            </style>
             <ul class="nav navbar-nav navbar-right">
                 <form class="form-inline navbar-form navbar-left">
                     <div class="form-group inline-form col-sm-1 col-md-12">
@@ -41,5 +90,3 @@ $page = explode("/", pagencours);
         </div>
     </div>
 </nav>
-<?php include 'modal_connexion.include.php' ?>
-<?php include 'modal_inscription.include.php' ?>
