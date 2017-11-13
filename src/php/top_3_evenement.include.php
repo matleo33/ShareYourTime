14951998e0fa8f58cca1d86e5d83786ccc6bbd9d
Mathieu <mathieu.leothaud@etu.u-bordeaux.fr>
2017-11-13 13:46:18 +0100

    TOP 3 EVENEMENT PRESQUE FONCTIONNEL

--- src/php/top_3_evenement.include.php
+++ src/php/top_3_evenement.include.php
@@ -1,53 +1,46 @@
 <?php
+
 //Faire fonction qui retourne les 3 plus gros évènements
-try
-{
-$bdd = new PDO('mysql:host=localhost;dbname=shareyourtime;charset=utf8', 'root', '');
-}
-catch (Exception $e)
-{
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=shareyourtime;charset=utf8', 'root', '');
+} catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
-function get_three_best_event() {
-    $events = array();
+
+function get_three_best_event($bdd) {
     $reponse = $bdd->query('SELECT * '
             . 'FROM events '
             . 'WHERE est_fini=FALSE '
-            . 'ORDER BY '/*Trouver ce qui permet d'ordonner*/.' '
             . 'LIMIT 0,2');
-while ($donnees = $reponse->fetch())
+    while ($donnees = $reponse->fetch()) {
+        display_event($donnees['nom'], 10, 10, "https://www.facebook.com", "https://www.bg.fr");
+    }
+    $reponse->closeCursor(); // Termine le traitement de la requête
+}
 
-{
-
+get_three_best_event($bdd);
 ?>
-    <p>
-    <strong>Prenom</strong> : <?php echo $donnees['prenom']; ?><br />
-    <strong>Nom</strong> :  <?php echo $donnees['nom']; ?>
-    </p>
 <?php
-}
-$reponse->closeCursor(); // Termine le traitement de la requête
-?>
-}
 
 //Faire fonction qui, avec les infos en parametre, affiche un évènement 
 function display_event(string $name, int $nb_people, int $price, string $facebook_link, string $ticketing_link) {
     echo "<div class='important_event'>"
     . "<div class='image'>"
-            . "<img src='" . /*Ici mettre code pour avoir image.*/ "' />"
-            . "</div>"
-            . "<div>"
-            . "<div>"
-            . $name
-            . "</div>"
-            . "<div>Nombre de covoiturages : " 
-            . $nb_people 
-            . "</div>"
-            . "<div>A partir : "
-            . $price
-            . "</div>"
-            . "<button>J'y vais"
-            . "</button>"
+    . "<img src='" . /* Ici mettre code pour avoir image. */ "' />"
+    . "</div>"
+    . "<div>"
+    . "<div>"
+    . $name
+    . "</div>"
+    . "<div>Nombre de covoiturages : "
+    . $nb_people
+    . "</div>"
+    . "<div>A partir de "
+    . $price
+    . " €"
+    . "</div>"
+    . "<button>J'y vais"
+    . "</button>"
     . "</div>"
     . "<div>"
     . "<ul><li>"
@@ -57,8 +50,7 @@ function display_event(string $name, int $nb_people, int $price, string $faceboo
 }
 
 //Appeler la fonction trois fois
-for ($int = 0; $int < 5; $int++)
-{
+for ($int = 0; $int < 3; $int++) {
     //display_event($name, $int, $price, $facebook_link, $ticketing_link);
 }
 ?>
