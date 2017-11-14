eaa56f3b1e2778837f192a9f969796fab8649357
Mathieu <mathieu.leothaud@etu.u-bordeaux.fr>
2017-11-14 13:29:22 +0100

    base page recherche

--- src/php/recherche.php
+++ src/php/recherche.php
@@ -1,8 +1,30 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    <head>
+        <meta charset="UTF-8">
+        <link href="./../css/style.css" rel="stylesheet">
 
+        <!-- Bootstrap-->
+        <link rel="stylesheet" href="./../BootStrap/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <script src="../js/recherche.js"></script>
+        <script src="../BootStrap/js/bootstrap.min.js"></script>
+
+        <title>Share Your Time</title>
+    </head>
+
+    <body>
+        <?php include 'navbar.include.php'; ?><br/>
+        <?php include 'modal_connexion.include.php' ?>
+        <?php include 'modal_inscription.include.php' ?>
+        <?php
+        /*
+         * To change this license header, choose License Headers in Project Properties.
+         * To change this template file, choose Tools | Templates
+         * and open the template in the editor.
+         */
+        ?>
+    </body>
+</html>
