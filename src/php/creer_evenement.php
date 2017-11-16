3e046eb8c763e0d2fa866d1143cf47cc608d35c6
yberwit <yberwit@gmail.com>
2017-11-16 19:25:05 +0100

    Ajout libraire DateTimePicker, complétion adresse avec Api google Maps créer événement

--- src/php/creer_evenement.php
+++ src/php/creer_evenement.php
@@ -11,6 +11,15 @@
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script src="../js/recherche.js"></script>
 
+    <!-- API Google -->
+    <script src="https://maps.googleapis.com/maps/api/js?libraries=places&amp;key=AIzaSyC5VG6abk2Aio0RS7MBAbbN8N-FZxTrD84" type="text/javascript"></script>
+
+
+    <!-- Library DateTimePicker -->
+    <link href="../BootStrap/css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
+    <script type="text/javascript" src="../BootStrap/js/bootstrap-datetimepicker.js" charset="UTF-8"></script>
+    <script type="text/javascript" src="../BootStrap/js/locales/bootstrap-datetimepicker.fr.js" charset="UTF-8"></script>
+
     <title>Share Your Time</title>
 </head>
 
@@ -20,20 +29,43 @@
     <h1 style="color: white">CREER EVENEMENT</h1>
     <form id="formRecherche">
         <div class="form-group">
-            <input class="form-control" type="text" name="eventName" id="eventName" placeholder="Nom de l'événement"/>
+            <input class="form-control" type="text" name="eventName" required="required" id="eventName" placeholder="Nom de l'événement"/>
         </div>
         <input type="submit" id="submitEventName" class="btn btn-primary" value="Suivant"/>
     </form>
     <span id="msg_all"></span>
 </div>
+
 <div class="container col-lg-offset-3 col-lg-6 col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8 col-xs-12" id="containerCreation" style="text-align: center;background-color: RGB(69,164,247);visibility: hidden">
     <form action="rechercheEvenement.php" method="post" id="formRechercheSuite">
         <div class="form-group">
             <input class="form-control" placeholder="Nom de l'événement"/>
+            <input class="form-control" id="adressAutoComplete" placeholder="Adresse evenement"/>
+        </div>
+        <div class='input-group date datetimepicker'>
+            <input type='text' class="form-control" placeholder="Date debut + heure"/>
+            <span class="input-group-addon">
+                <span class="glyphicon glyphicon-calendar"></span>
+            </span>
+        </div>
+        <div class='input-group date datetimepicker'>
+            <input type='text' class="form-control" placeholder="Date fin + heure"/>
+            <span class="input-group-addon">
+                <span class="glyphicon glyphicon-calendar"></span>
+            </span>
         </div>
         <input type="submit" class="btn btn-primary" value="Créer"/>
     </form>
 </div>
+
+
+<script type="text/javascript">
+    $(function () {
+        $('.datetimepicker').datetimepicker({
+            locale: 'fr'
+        });
+    });
+</script>
 
 <script>
     $(document).ready(function(e) {
@@ -56,6 +88,18 @@
     });
 </script>
 
+<script>
+    function initializeAutocomplete(id) {
+        var element = document.getElementById(id);
+        if (element) {
+            var autocomplete = new google.maps.places.Autocomplete(element, { types: ['geocode'] });
+            google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
+        }
+    }
+    google.maps.event.addDomListener(window, 'load', function() {
+        initializeAutocomplete('adressAutoComplete');
+    });
+</script>
 
 <script src="../BootStrap/js/bootstrap.min.js"></script>
 </body>
