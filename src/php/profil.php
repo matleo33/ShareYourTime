befc0203f3bdb52f6354b848dec08bb03c8324e1
Mathieu <mathieu.leothaud@etu.u-bordeaux.fr>
2017-11-17 19:09:28 +0100

    Ajout fichier profil

---
+++ src/php/profil.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="./../css/style.css" rel="stylesheet">
+
+    <!-- Bootstrap-->
+    <link rel="stylesheet" href="./../BootStrap/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="../js/recherche.js"></script>
+
+    <title>Share Your Time</title>
+</head>
+
+<body>
+<?php include 'navbar.include.php'; ?><br/>
+<!-- Photo de profil -->    
+<div class="col-sm-4">
+    <img src="" alt="photoProfil" />
+</div>
+<!-- Informations -->    
+<div class="col-sm-4">
+    <h1>Nom Prénom</h1>
+    <p>adresse@email.com</p>
+    <p>06 06 xx xx xx</p>
+    <p>Validation paypal</p>
+    <p>Nombre de trajets en tant que conducteur : </p>
+    <p>Nombre de trajets en tant que passager : </p>
+    <p>Nombre d'événements organisés : </p>
+    <p>Note sous forme d'étoiles</p>
+    <p>Description modifiable</p>
+</div>
+<!-- Caractéristiques trajets -->    
+<div class="col-sm-4">
+    <h1>Caractéristiques trajets</h1>
+    <p>Scrollbar entre timide et bavard</p>
+    <p>Animaux autorisés : décision</p>
+    <p>Fumeurs autorisés : décision</p>
+    <p>Musique autorisée : décision</p>
+    <h1>Derniers Avis</h1>
+    <img src="" alt="photo utilisateur" />
+    <p>Note sous forme d'étoiles</p>
+    <p>Commentaires</p>
+    <img src="" alt="photo utilisateur" />
+    <p>Note sous forme d'étoiles</p>
+    <p>Commentaires</p>
+</div>
+
+    <?php include 'footer.include.php'; ?>
+
+<script src="../BootStrap/js/bootstrap.min.js"></script>
+</body>
+
+
+</html>
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
