0ddf5d5f124f0c661a842c283a6fec4431ffaaa8
Mathieu <mathieu.leothaud@etu.u-bordeaux.fr>
2017-11-22 15:17:14 +0100

    J'ai rien fait mdr

--- src/php/proposer_trajet.php
+++ src/php/proposer_trajet.php
@@ -1,42 +1,51 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-<head>
-    <meta charset="UTF-8">
-    <link href="./../css/style.css" rel="stylesheet">
+    <head>
+        <meta charset="UTF-8">
+        <link href="./../css/style.css" rel="stylesheet">
 
-    <!-- Bootstrap-->
-    <link rel="stylesheet" href="./../BootStrap/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <!-- Bootstrap-->
+        <link rel="stylesheet" href="./../BootStrap/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 
-    <title>Share Your Time</title>
-</head>
+        <title>Share Your Time</title>
+    </head>
 
-<body>
-<?php include 'navbar.include.php'; ?>
+    <body>
+        <?php include 'navbar.include.php'; ?>
 
-<div class="container col-lg-offset-3 col-lg-6 col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8 col-xs-12" style="text-align: center;background-color: RGB(69,164,247)">
-    <h1 style="color: white">PROPOSER UN TRAJET</h1>
-    <form action="proposer_trajet_envoi.php" method="post" id="formTrajet">
-        
-            <table class="table-bordered">
+        <div class="container col-lg-offset-3 col-lg-6 col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8 col-xs-12" style="text-align: center;background-color: RGB(69,164,247)">
+            <h1 style="color: white">PROPOSER UN TRAJET</h1>
+            <form action="proposer_trajet_envoi.php" method="post" id="formTrajet">
 
-                <tr>
-                    <td>Ville de depart</td>
-                    <td rowspan="2">Date + heure</td>
-                </tr>
-                <tr>
-                    <td>Lieu du rdv</td>
-                </tr>
-            </table>
-        
-            <input type="submit" id="submitEventName" class="btn btn-primary" value="Suivant"/>
-        </form>
-</div>
+                <table class="table-bordered">
+
+                    <tr>
+                        <td>Ville de depart</td>
+                        <td rowspan="2">Date + heure</td>
+                    </tr>
+                    <tr>
+                        <td>Lieu du rdv</td>
+                    </tr>
+                </table>
+                <table class="table-bordered">
+                    <tr>
+                        <td>Ville d'arrivée</td>
+                        <td rowspan="2">Date + heure</td>
+                    </tr>
+                    <tr>
+                        <td>Lieu du point d'arrivée</td>
+                    </tr>
+                </table>
+
+                <input type="submit" id="submitEventName" class="btn btn-primary" value="Suivant"/>
+            </form>
+        </div>
 
 
 
-<script src="../BootStrap/js/bootstrap.min.js"></script>
-</body>
+        <script src="../BootStrap/js/bootstrap.min.js"></script>
+    </body>
 
 </html>
