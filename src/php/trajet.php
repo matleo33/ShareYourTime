f5d8b6e2778d08b9de1407dd54dbe53f93117a27
Mathieu <mathieu.leothaud@etu.u-bordeaux.fr>
2017-12-05 11:26:27 +0100

    FUCK

--- src/php/trajet.php
+++ src/php/trajet.php
@@ -29,60 +29,93 @@
             $reponse = $bdd->query('SELECT *, COUNT(*) '
                     . 'FROM trajet INNER JOIN users on trajet.chauffeur = users.id_users INNER JOIN etape on trajet.id_trajet = etape.trajet '
                     . 'WHERE id_trajet = ' . $_GET['id_trajet'])
-                    ; ' GROUP BY trajet.id_trajet';
+            ;
+            ' GROUP BY trajet.id_trajet';
             while ($donnees = $reponse->fetch()) {
+                echo "<div class=\"nomTrajet\">";
                 echo "<div class=\"text-center col-sm-12\">";
                 echo "<h1>" . $donnees['ville_depart'] . " - " . $donnees['ville_arrivee'] . ' ' . $donnees['prix_tot'] . " €</h1>";
-                ?>
-        <div class="col-sm-3">
-        </div>
-        <div class="col-sm-6">
-            <div class="col-sm-6">
-                <img src="ba" alt="photo chauffeur" />
-            </div>
-            <div class="col-sm-6">
-                <p><?php echo $donnees['prenom'] . ' ' .$donnees['nom'];?></p>
-                <p><?php echo $donnees['num_telephone'];?></p>
-                <p><?php echo $donnees['mail'];?></p>
-                <p><?php
-                                        for ($i = 0; $i < $donnees['personnalite']; ++$i) {
-                                            echo '★';
-                                        }
-                                        for ($j = 0; $j < 10 - $donnees['personnalite']; ++$j) {
-                                            echo '☆';
-                                        }
-                                        ?></p>
-            </div>
-            <div class="col-sm-6">
-                <div class="col-sm-4">
-                    Ville de départ
-                </div>
-                <div class="col-sm-8">
-                    <p>Lieu de rdv</p>
-                    <p>Heure de rdv</p>
-                </div>
-                <?php for($i = 0; $i < $donnees['COUNT(*)']; ++$i) { ?>
-                <div class="col-sm-4">
-                    Ville etape
-                </div>
-                <div class="col-sm-8">
-                    <p>Lieu de rdv</p>
-                    <p>Heure de rdv</p>
-                </div>
-                <?php } ?>
-            </div>
-            <div class="col-sm-6">
-                Retard toléré : 
-                Pensez à me contacter pour plus d'informations !
-            </div>
-        </div>
-        <div class="col-sm-3">
-        </div>
-        <?php
                 echo "</div>";
+                echo "</div>";
+                ?>
+                <div class="infosTrajet">
+                    <div class="col-sm-2">
+                    </div>
+                    <div class="col-sm-8 text-center">
+                        <div class="col-sm-6">
+                            <img src="ba" alt="photo chauffeur" />
+                        </div>
+                        <div class="col-sm-6">
+                            <p><?php echo $donnees['prenom'] . ' ' . $donnees['nom']; ?></p>
+                            <p><?php echo $donnees['num_telephone']; ?></p>
+                            <p><?php echo $donnees['mail']; ?></p>
+                            <p><?php
+                                for ($i = 0; $i < $donnees['personnalite']; ++$i) {
+                                    echo '★';
+                                }
+                                for ($j = 0; $j < 10 - $donnees['personnalite']; ++$j) {
+                                    echo '☆';
+                                }
+                                ?></p>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="col-sm-12">
+                                <div class="col-sm-4 aroundedText">
+                                    Ville de départ
+                                </div>
+                                <div class="col-sm-8">
+                                    <p>Lieu de rdv</p>
+                                    <p>Heure de rdv</p>
+                                </div>
+                            </div>
+                            <?php for ($i = 0; $i < $donnees['COUNT(*)']; ++$i) { ?>
+                                <div class="col-sm-12">
+                                    <div class="col-sm-4 aroundedText">
+                                        Ville etape
+                                    </div>
+                                    <div class="col-sm-8">
+                                        <p>Lieu de rdv</p>
+                                        <p>Heure de rdv</p>
+                                    </div>
+                                </div>
+                            <?php } ?>
+                            <div class="col-sm-12">
+                                <div class="col-sm-4 aroundedText">
+                                    Ville d'arrivée
+                                </div>
+                                <div class="col-sm-8">
+                                    <p>Lieu de rdv</p>
+                                    <p>Heure d'arrivée</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6" style="height: inherit; border: black solid 1px;">
+                            <p>Retard toléré : <?php echo $donnees['retard']; ?> minutes</p>
+                            <p>Pensez à me contacter pour plus d'informations !</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-2">
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="col-sm-2">
+                            Autoroute
+                        </div>
+                        <div class="col-sm-4">
+                            Il reste <?php echo $donnees['nb_place']; ?> place<?php if ($donnees['nb_place'] > 1) echo 's'; ?>
+                        </div>
+                        <div class="col-sm-5">
+                            <input type="number" min="0" max="<?php echo $donnees['nb_place']; ?>" style="width: 50px;" />
+                            <button>Je reserve</button>
+                        </div>
+                        <div class="col-sm-1">
+                            <button>Signaler</button>
+                        </div>
+                    </div>
+                </div>
+                <?php
             }
         } else {
-            echo "<div class=\"text-center\">";
+            echo "<div class=\"text-center nomTrajet\">";
             echo "<h2>Désolé, nous n'avons pas pu trouver le trajet en question, merci de vous rediriger "
             . "vers la page d'accueil.</h2>";
             echo "<a href=\"index.php\"><button>Cliquez ici</button></a>";
