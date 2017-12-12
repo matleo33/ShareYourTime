04efd967b55659f874fceebdfb3bba49099b7a43
yberwit <yberwit@gmail.com>
2017-12-12 10:11:46 +0100

    Proposer trajet peut ajouter des étapes, et liaison avec creer_evenement

--- src/php/proposer_trajet.php
+++ src/php/proposer_trajet.php
@@ -24,10 +24,10 @@
 
     <body>
         <?php include 'navbar.include.php'; ?>
-
+<div class="container-fluid">
         <div class="container col-lg-offset-3 col-lg-6 col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8 col-xs-12" id="divProposerTrajet">
             <h1 id="titreBlanc">PROPOSER UN TRAJET</h1>
-            <form action="proposer_trajet_envoi.php" method="post" id="formTrajet">
+            <form method="post" id="formTrajet">
                 <?php
                 $nomEvenement = NULL;
                 $villeEvenement = NULL;
@@ -62,9 +62,11 @@
                     }
                     ?>"/>
                 </div>
-                <input type="submit" id="submitEventName" class="btn btn-primary" value="Suivant"/>
-            </form>
-            <form>
+                <input type="submit" id="submitEventName" class="btn btn-primary" value="Suivant" onclick="verification_event()"/>
+                </form>
+        </div>
+    <div class="container col-lg-offset-3 col-lg-6 col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8 col-xs-12" id="divInfoTrajet" style="text-align: center;background-color: RGB(69, 164, 247);visibility: hidden;">
+                <form id="information_trajet_form" >
                 <div class="col-sm-12">
                     <div class="col-sm-10 col-sm-offset-1 groupeDateLieu">
                         <div class="col-sm-8">
@@ -111,25 +113,26 @@
                             <div class="col-sm-10" id="ajoutEtape"> 
                                 Ajouter une étape
                             </div>
-                            <span onclick="ajouterEtape()" class="input-group-addon">
+                            <span onclick="afficherModalAjoutEtape()" class="input-group-addon" style="cursor: pointer;">
                                 <span class="glyphicon glyphicon-plus"></span>
                             </span>
                         </div>
+                        <div>
+                            <table class="table table-bordered" style="background-color: white">
+                                <thead>
+                                <tr>
+                                    <th>Ville</th>
+                                    <th>Lieu</th>
+                                    <th>Heure</th>
+                                </tr>
+                                </thead>
+                                <tbody id="tableau_etape">
+
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
-                    <div class="col-sm-10 col-sm-offset-1 groupeDateLieu">
-                        <div class="col-sm-8">
-                            <input class="form-control" type="text" name="villeDepart" required="required" id="villeDepart" placeholder="Ville etape"/>
-                        </div>
-                        <div class='col-sm-4 input-group date datetimepicker'>
-                            <input type='text' id="date_passage" name="date_passage" class="form-control" required="required" placeholder="Heure passage" />
-                            <span class="input-group-addon">
-                                <span class="glyphicon glyphicon-calendar"></span>
-                            </span>
-                        </div>
-                        <div class="col-sm-8">
-                            <input class="form-control" type="text" name="lieuPassage" required="required" id="lieuPassage" placeholder="Lieu de passage"/>
-                        </div>
-                    </div>
+
                     <div class="col-sm-10 col-sm-offset-1 infosComplementairesProposerTrajet">
                         <div class="col-sm-6">
                             <input class="form-control elementInfoComp" type="number" min="1" name="nbPlaces" required="required" id="nbPlaces" placeholder="Nombre de places"/>
@@ -149,9 +152,42 @@
                         </div>
                     </div>
                 </div>
-                <input type="submit" id="submitEventName" class="btn btn-primary" value="Créer trajet"/>
+                <input type="submit" id="submitTrajet" class="btn btn-primary" value="Créer trajet"/>
             </form>
 
+        </div>
+</div>
+
+        <div class="modal fade" id="modalAjoutEtape" role="dialog" data-keyboard="false" data-backdrop="static">
+            <div class="modal-dialog">
+
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Ajout d'une étape</h4>
+                    </div>
+                    <form>
+                        <div class="form-group col-sm-10 col-sm-offset-1" style="margin-top: 5px">
+                            <input class="form-control" type="text" name="ville_depart" required="required" id="ville_depart" placeholder="Ville etape"/>
+                        </div>
+                        <div class="form-group col-sm-10 col-sm-offset-1">
+                            <input class="form-control" type="text" name="lieuPassage" required="required" id="lieuPassage" placeholder="Lieu de passage"/>
+                        </div>
+                        <div class='form-group col-sm-10 col-sm-offset-1 input-group date datetimepicker'>
+                            <input type='text' id="date_passage" name="date_passage" class="form-control" required="required" placeholder="Heure passage" />
+                            <span class="input-group-addon">
+                                <span class="glyphicon glyphicon-calendar"></span>
+                            </span>
+                        </div>
+
+                        <div class="modal-footer">
+                            <input type="submit" class="btn btn-default center-block" onclick="ajouterEtape();return false;" value="Ajouter l'étape" id="inputConnexion"/>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
         </div>
 
         <?php include('footer.include.php'); ?>
@@ -159,3 +195,62 @@
     </body>
 
 </html>
+
+<script>
+    var etape = [];
+    var compteur = 0;
+    function afficherModalAjoutEtape() {
+        $("#modalAjoutEtape").modal('show');
+    }
+    function ajouterEtape() {
+        var newEtape = [];
+        //var id_ligne_tr = "tr"+compteur;
+        newEtape.push(document.getElementById("ville_depart").value,document.getElementById("lieuPassage").value,document.getElementById("date_passage").value)
+        etape.push(newEtape);
+        document.getElementById("tableau_etape").innerHTML += '<tr id="tr'+compteur+'"><td>'+document.getElementById("ville_depart").value+
+            '</td><td>'+document.getElementById("lieuPassage").value+
+            '</td><td>'+document.getElementById("date_passage").value+'</td><td>' +
+             '<input type="button" onclick="supprimer_etape('+compteur+')"/></td></tr>';
+
+    document.getElementById("ville_depart").value = '';
+        document.getElementById("lieuPassage").value = '';
+        document.getElementById("date_passage").value = '';
+        $("#modalAjoutEtape").modal('hide');
+        compteur++;
+    }
+    function supprimer_etape(id) {
+        var obj = document.getElementById("tableau_etape");
+        var old = document.getElementById("tr"+id);
+
+        obj.removeChild(old);
+        delete etape[id];
+    }
+
+    function verification_event() {
+        $(document).ready(function(e) {
+            $("#formTrajet").submit(function () {
+                $.get("rechercheEvenement.php",$(this).serialize(),function(id){
+                    if(id != '')
+                    {
+                        $("#divInfoTrajet").css("visibility", "visible");
+                        $("#eventName").attr("disabled","disabled");//TODO Faire avec readonly au lieu de disabled
+                        eventName = $("#eventName").val();
+
+                        document.getElementById("formTrajet").removeChild(document.getElementById("submitEventName"));
+                    }
+                    else
+                    {
+                        var currentLocation =  document.location.href;
+                        currentLocation = currentLocation.substring( 0 ,currentLocation.lastIndexOf( "src" ) );
+                        currentLocation += 'src/php/creer_evenement.php?nom_event=' + $("#eventName").val();
+                        window.location.href = currentLocation ;
+                    }
+
+                });
+                return false; // permet de ne pas recharger la page
+            });
+        });
+
+    }
+
+</script>
--- src/php/recherche.php
+++ src/php/recherche.php
@@ -161,7 +161,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Pas d'événement correspondant</h4>
+                    <h4 class="modal-title">Pas d'événenement correspondant</h4>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -185,7 +185,6 @@
             {
                 data_get+="&nom_event=" + nom_event;
             }
-            alert(data_get);
             $.ajax({
                 type:"GET",
                 data: data_get,//$("#optionAvanceeForm").serialize() + "&nom_event=" +$("#nom_event").value,
