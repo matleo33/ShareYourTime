98dadf6efd800d7484e1d9d3e20a5cede58dc456
unknown <amaury2485@gmail.com>
2018-01-07 18:56:52 +0100

    Corrections en xs

--- src/php/getTrajets.php
+++ src/php/getTrajets.php
@@ -1,16 +1,17 @@
 <?php
 include 'getEtapes.php';
 
-function getTrajets($bdd, string $id_event, $page) {
+function getTrajets($bdd, string $id_event, $page)
+{
     if (isset($_GET['page'])) {
         $page = $_GET['page'];
     } else {
         $page = 0;
     }
     $reponseTrajets = $bdd->query('SELECT id_trajet, lien_photo, id_users, nom, prenom, ville_depart, lieu_depart, ville_arrivee, lieu_arrive, prix_tot, personnalite, autoroute '
-            . 'FROM trajet INNER JOIN users on trajet.chauffeur = users.id_users '
-            . 'WHERE evenement=\'' . $id_event . '\' '
-            . 'LIMIT ' . ($page * 2) . ',2');
+        . 'FROM trajet INNER JOIN users on trajet.chauffeur = users.id_users '
+        . 'WHERE evenement=\'' . $id_event . '\' '
+        . 'LIMIT ' . ($page * 2) . ',2');
     while ($donneesTrajet = $reponseTrajets->fetch()) {
         $note = 5;
         $hasNote = getHasNote($bdd, $donneesTrajet['id_users']);
@@ -19,42 +20,45 @@ function getTrajets($bdd, string $id_event, $page) {
         }
         ?>
         <div class="col-sm-12" id="divCovoiturage">
-            <div class="col-xs-12 col-sm-12 col-lg-2">
+            <div class="col-xs-12 col-sm-12 col-lg-2" style="margin-top: 10px">
                 <?php if ($donneesTrajet['lien_photo'] == NULL) { ?>
-                    <img class="photoProfilTrajet" src="../img/imageProfil2.PNG" alt="photoProfil" />
+                    <img class="photoProfilTrajet" src="../img/imageProfil2.PNG" alt="photoProfil"/>
                     <?php
                 } else {
-                    ?> 
-                    <img class="photoProfilTrajet" src="../../images/<?php echo $donneesTrajet['lien_photo'] ?>" alt="photoProfil" />
+                    ?>
+                    <img class="photoProfilTrajet" src="../../images/<?php echo $donneesTrajet['lien_photo'] ?>"
+                         alt="photoProfil"/>
                     <?php
                 }
                 ?>
             </div>
-            <div class="col-lg-8 col-lg-offset-1 col-sm-12 col-xs-12 trajetEvenement">
+            <div class="col-lg-8 col-lg-offset-1 col-sm-12 col-xs-12 trajetEvenement" style="margin-top: 30px">
                 <div class="col-sm-6">
-                    <span class="nomChauffeurTrajetEvenement">
+                    <span class="nomChauffeurTrajetEvenement" style="margin-left: 100px">
                         <?php
                         echo $donneesTrajet['nom'] . ' ' . $donneesTrajet['prenom'];
                         ?>
                     </span>
-                    <p>Depart : <?php echo $donneesTrajet['ville_depart'] . ', ' . $donneesTrajet['lieu_depart']; ?></p>
-                    <p>Arrivée : <?php echo $donneesTrajet['ville_arrivee'] . ', ' . $donneesTrajet['lieu_arrive']; ?></p>
+                    <p>Depart
+                        : <?php echo $donneesTrajet['ville_depart'] . ', ' . $donneesTrajet['lieu_depart']; ?></p>
+                    <p>Arrivée
+                        : <?php echo $donneesTrajet['ville_arrivee'] . ', ' . $donneesTrajet['lieu_arrive']; ?></p>
                     <p>Prix : <?php echo $donneesTrajet['prix_tot'] . ' €'; ?></p>
                     <p>Note chauffeur : <?php
                         if ($hasNote) {
-                            ?>
-                            <span style="font-size:150%; color:
+                        ?>
+                        <span style="font-size:150%; color:
                             <?php
-                            if ($note >= 9) {
-                                echo 'gold;';
-                            } else if ($note >= 7) {
-                                echo 'silver;';
-                            } else if ($note >= 5)
-                                echo '#614E1A;';
-                            else {
-                                echo 'red;';
-                            }
-                            ?>">
+                        if ($note >= 9) {
+                            echo 'gold;';
+                        } else if ($note >= 7) {
+                            echo 'silver;';
+                        } else if ($note >= 5)
+                            echo '#614E1A;';
+                        else {
+                            echo 'red;';
+                        }
+                        ?>">
                                       <?php
                                       for ($i = 0; $i < $note; ++$i) {
                                           echo '★';
@@ -64,7 +68,7 @@ function getTrajets($bdd, string $id_event, $page) {
                                       }
                                       ?>
                             </span>
-                            <?php
+                        <?php
                         } else {
                             echo 'Inconnue';
                         }
@@ -75,7 +79,7 @@ function getTrajets($bdd, string $id_event, $page) {
                     <?php
                     if ($donneesTrajet['autoroute']) {
                         ?>
-                        <img class="logoAutoroute" src="../img/autoroute.png" alt="autouroute : oui" />
+                        <img class="logoAutoroute" src="../img/autoroute.png" alt="autouroute : oui"/>
                         <?php
                     }
                     ?>
