ffad3f76ff2d17ce17618c2d4a4792179b64bab8
unknown <amaury2485@gmail.com>
2018-01-10 15:42:38 +0100

    Modification age -> date de naissance

--- src/php/traiter_inscription.php
+++ src/php/traiter_inscription.php
@@ -2,7 +2,7 @@
 
 $origin = $_SERVER["HTTP_REFERER"];
 $pageorigin = explode("/", $origin);
-if(end($pageorigin) == "") {
+if (end($pageorigin) == "") {
     array_push($pageorigin, "index.php");
 }
 
@@ -16,27 +16,32 @@ $name = htmlspecialchars($_POST["inputName"]);
 $email = htmlspecialchars($_POST["inputMail"]);
 $pass = htmlspecialchars($_POST["inputPassword"]);
 $checkPassword = htmlspecialchars($_POST["inputCheckPassword"]);
-$age = htmlspecialchars($_POST["inputAge"]);
+//$age = htmlspecialchars($_POST["inputAge"]);
 $number = htmlspecialchars($_POST["inputNumber"]);
 $type = htmlspecialchars($_POST["inputType"]);
 $address = htmlspecialchars($_POST["inputAddress"]);
 $pc = htmlspecialchars($_POST["inputPC"]);
 $city = htmlspecialchars($_POST["inputCity"]);
 $phoneNumber = htmlspecialchars($_POST["inputPhoneNumber"]);
+$birthDate = htmlspecialchars($_POST["inputBirthDate"]);
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, array (PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
 
-    $stmt = $conn->prepare("INSERT INTO `users` (`nom`, `prenom`, `mail`, `mot_de_passe`, `age`,
+    $stmt = $conn->prepare("INSERT INTO `users` (`nom`, `prenom`, `mail`, `mot_de_passe`, `date_naissance`,
  `numero_adresse`, `type_adresse`, `nom_adresse`, `cp`, `ville`, `num_telephone`) 
   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
-    $stmt->execute(array($name, $firstName, $email, $pass, $age, $number, $type, $address, $pc, $city, $phoneNumber));
+    $stmt->execute(array($name, $firstName, $email, $pass, $birthDate, $number, $type, $address, $pc, $city, $phoneNumber));
+
+    echo("INSERT INTO `users` (`nom`, `prenom`, `mail`, `mot_de_passe`, `date_naissance`,
+ `numero_adresse`, `type_adresse`, `nom_adresse`, `cp`, `ville`, `num_telephone`) 
+  VALUES (" . $name . ", " . $firstName . ", " . $email . ", " . $pass . ", " . $birthDate . ", " . $number . ", " . $type . ", " . $address . ", " . $pc . ", " . $city . ", " . $phoneNumber . ");");
 
     session_start();
     $conn = null;
 
-    $url = end($pageorigin);
-    header("Location: " . $url);
+    //$url = end($pageorigin);
+    //header("Location: " . $url);
 
     /*
     $res = $stmt->fetch();
