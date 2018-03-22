e8243d3d5f3fd63f81b0626d7a8e197279bc0fbb
Mathieu <mathieu.leothaud@etu.u-bordeaux.fr>
2018-03-22 15:57:53 +0100

    Init Connec ftp

--- src/php/upload_photo.php
+++ src/php/upload_photo.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-//include("ftp.php"); //TODO inclure le fichier contentant les variables de connexion au ftp
+include("ftp.php"); //TODO inclure le fichier contentant les variables de connexion au ftp
 
 // Initialisation des variables à propos de la photo
 $dossier = '../../images/';
