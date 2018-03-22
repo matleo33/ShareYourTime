a2f90eab7402ff0c03f82e32ad172a5e9fdab835
unknown <amaury2485@gmail.com>
2018-03-22 15:20:14 +0100

    Ajout encryptage mdp

--- src/php/traiter_connexion.php
+++ src/php/traiter_connexion.php
@@ -12,27 +12,28 @@ $servername = "localhost";
 $dbname = "shareyourtime";
 $username = "root";
 $password = "D0nald&Ch@uve";
+
 $email = htmlspecialchars($_GET["inputEmailConnexion"]);
 $pass = htmlspecialchars($_GET["inputPasswordConnexion"]);
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, array (PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
 
-    $stmt = $conn->prepare("SELECT id_users, prenom FROM users WHERE mail= ? AND  mot_de_passe= ?");
-    $stmt->execute(array($email, $pass));
+    $stmt = $conn->prepare("SELECT id_users, prenom, mot_de_passe FROM users WHERE mail= ?");
+    $stmt->execute(array($email));
 
     $res = $stmt->fetch();
-    if ($res) {
+    if (password_verify($pass, $res[2])) {
         $_SESSION["ID_USER"] = $res[0];
         setcookie('NOM_USER', $res[1]);
         $_COOKIE["NOM_USER"] = $res[1];
         echo $res[0];
-        header("Location: ".$url);
+        //header("Location: ".$url);
     } else {
         echo "0";
     }
 } catch (PDOException $e) {
-    header("Location: ".$url);
+    //header("Location: ".$url);
 }
 $conn = null;
 
