5fb478019515b622592649196b0544b1702210b7
Mathieu <mathieu.leothaud@etu.u-bordeaux.fr>
2018-05-11 19:14:42 +0200

    Coucou

--- src/php/modal_inscription.include.php
+++ src/php/modal_inscription.include.php
@@ -10,45 +10,45 @@
             </div>
             <!-- Formulaire d'inscription -->
             <form method="post" action="traiter_inscription.php">
-                <div class="form-group col-sm-10 col-sm-offset-1" id="divFirstName">
+                <div class="form-group col-sm-10 col-sm-offset-1" id="divFirstName"><span style="color:#F62">*</span>
                     <input type="text" class="form-control" id="inputFirstName" name="inputFirstName"
                            placeholder="Prénom" required>
                 </div>
-                <div class="form-group col-sm-10 col-sm-offset-1">
+                <div class="form-group col-sm-10 col-sm-offset-1"><span style="color:#F62">*</span>
                     <input type="text" class="form-control" id="inputName" name="inputName" placeholder="Nom" required>
                 </div>
-                <div class="form-group col-sm-10 col-sm-offset-1">
+                <div class="form-group col-sm-10 col-sm-offset-1"><span style="color:#F62">*</span>
                     <input type="email" class="form-control" id="inputMail" name="inputMail"
                            placeholder="prenom.nom@etu.u-bordeaux.fr" required>
                 </div>
-                <div class="form-group col-sm-10 col-sm-offset-1">
+                <div class="form-group col-sm-10 col-sm-offset-1"><span style="color:#F62">*</span>
                     <input type="password" class="form-control" id="inputPassword" name="inputPassword" pattern='^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&?"]).*$'
                            placeholder="Mot de passe" required>
                 </div>
-                <div class="form-group col-sm-10 col-sm-offset-1">
+                <div class="form-group col-sm-10 col-sm-offset-1"><span style="color:#F62">*</span>
                     <input type="password" class="form-control" id="inputCheckPassword" name="inputCheckPassword"
                            placeholder="Répétez votre mot de passe" required>
                 </div>
-                <div class="form-group col-sm-4 col-sm-offset-1">
+                <div class="form-group col-sm-4 col-sm-offset-1"><span style="color:#F62">*</span>
                     <input type="text" class="form-control" id="inputNumber" name="inputNumber" placeholder="N°"
                            required>
                 </div>
-                <div class="form-group col-sm-6">
+                <div class="form-group col-sm-6"><span style="color:#F62">*</span>
                     <input type="text" class="form-control" id="inputType" name="inputType" placeholder="Type rue"
                            required>
                 </div>
-                <div class="form-group col-sm-10 col-sm-offset-1">
+                <div class="form-group col-sm-10 col-sm-offset-1"><span style="color:#F62">*</span>
                     <input type="text" class="form-control" id="inputAddress" name="inputAddress" placeholder="Adresse"
                            required>
                 </div>
-                <div class="form-group col-sm-4 col-sm-offset-1">
+                <div class="form-group col-sm-4 col-sm-offset-1"><span style="color:#F62">*</span>
                     <input type="text" class="form-control" id="inputPC" name="inputPC" placeholder="CP" required>
                 </div>
-                <div class="form-group col-sm-6">
+                <div class="form-group col-sm-6"><span style="color:#F62">*</span>
                     <input type="text" class="form-control" id="inputCity" name="inputCity" placeholder="Ville"
                            required>
                 </div>
-                <div class="form-group col-sm-10 col-sm-offset-1">
+                <div class="form-group col-sm-10 col-sm-offset-1"><span style="color:#F62">*</span>
                     <input type="tel" class="form-control" id="inputPhoneNumber" name="inputPhoneNumber"
                            placeholder="Numéro de téléphone" required>
                 </div>
@@ -58,6 +58,7 @@
                 <div class="modal-footer">
                     <input type="submit" class="btn btn-default center-block" value="Inscription"
                            id="inputInscription"/>
+                    <span style="color:#F62">* : obligatoire</span>
                 </div>
             </form>
         </div>
