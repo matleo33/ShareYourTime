7bce766b9d230e5524a2b297923b79a0ac758b67
Mathieu <mathieu.leothaud@etu.u-bordeaux.fr>
2018-05-11 19:44:19 +0200

    coucou

--- src/php/modal_inscription.include.php
+++ src/php/modal_inscription.include.php
@@ -10,48 +10,76 @@
             </div>
             <!-- Formulaire d'inscription -->
             <form method="post" action="traiter_inscription.php">
-                <div class="form-group col-sm-10 col-sm-offset-1" id="divFirstName"><span style="color:#F62">*</span>
+                <div class="form-group col-sm-10 col-sm-offset-1" id="divFirstName">
+                    <div class="col-sm-1" style="color:#F62">*</div>
                     <input type="text" class="form-control" id="inputFirstName" name="inputFirstName"
                            placeholder="Prénom" required>
                 </div>
                 <div class="form-group col-sm-10 col-sm-offset-1">
-                    <div class="col-sm-2" style="color:#F62">*</div>
-                    <div class="col-sm-10"><input type="text" class="form-control col-sm-10" id="inputName" name="inputName" placeholder="Nom" required></div>
+                    <div class="col-sm-1" style="color:#F62">*</div>
+                    <div class="col-sm-11"><input type="text" class="form-control col-sm-10" id="inputName" name="inputName" placeholder="Nom" required></div>
                 </div>
                 <div class="form-group col-sm-10 col-sm-offset-1"><span style="color:#F62">*</span>
-                    <input type="email" class="form-control" id="inputMail" name="inputMail"
-                           placeholder="prenom.nom@etu.u-bordeaux.fr" required>
+                    <div class="col-sm-11">
+                        <div class="col-sm-1" style="color:#F62">*</div>
+                        <input type="email" class="form-control" id="inputMail" name="inputMail"
+                               placeholder="prenom.nom@etu.u-bordeaux.fr" required>
+                    </div>
                 </div>
                 <div class="form-group col-sm-10 col-sm-offset-1"><span style="color:#F62">*</span>
-                    <input type="password" class="form-control" id="inputPassword" name="inputPassword" pattern='^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&?"]).*$'
-                           placeholder="Mot de passe" required>
+                    <div class="col-sm-11">
+                        <div class="col-sm-1" style="color:#F62">*</div>
+                        <input type="password" class="form-control" id="inputPassword" name="inputPassword" pattern='^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&?"]).*$'
+                               placeholder="Mot de passe" required>
+                    </div>
                 </div>
                 <div class="form-group col-sm-10 col-sm-offset-1"><span style="color:#F62">*</span>
-                    <input type="password" class="form-control" id="inputCheckPassword" name="inputCheckPassword"
-                           placeholder="Répétez votre mot de passe" required>
+                    <div class="col-sm-11">
+                        <div class="col-sm-1" style="color:#F62">*</div>
+                        <input type="password" class="form-control" id="inputCheckPassword" name="inputCheckPassword"
+                               placeholder="Répétez votre mot de passe" required>
+                    </div>
                 </div>
                 <div class="form-group col-sm-4 col-sm-offset-1"><span style="color:#F62">*</span>
-                    <input type="text" class="form-control" id="inputNumber" name="inputNumber" placeholder="N°"
-                           required>
+                    <div class="col-sm-11">
+                        <div class="col-sm-1" style="color:#F62">*</div>
+                        <input type="text" class="form-control" id="inputNumber" name="inputNumber" placeholder="N°"
+                               required>
+                    </div>
                 </div>
                 <div class="form-group col-sm-6"><span style="color:#F62">*</span>
-                    <input type="text" class="form-control" id="inputType" name="inputType" placeholder="Type rue"
-                           required>
+                    <div class="col-sm-11">
+                        <div class="col-sm-1" style="color:#F62">*</div>
+                        <input type="text" class="form-control" id="inputType" name="inputType" placeholder="Type rue"
+                               required>
+                    </div>
                 </div>
-                <div class="form-group col-sm-10 col-sm-offset-1"><span style="color:#F62">*</span>
-                    <input type="text" class="form-control" id="inputAddress" name="inputAddress" placeholder="Adresse"
-                           required>
+                <div class="form-group col-sm-10 col-sm-offset-1">
+                    <div class="col-sm-11">
+                        <div class="col-sm-1" style="color:#F62">*</div>
+                        <input type="text" class="form-control" id="inputAddress" name="inputAddress" placeholder="Adresse"
+                               required>
+                    </div>
                 </div>
-                <div class="form-group col-sm-4 col-sm-offset-1"><span style="color:#F62">*</span>
-                    <input type="text" class="form-control" id="inputPC" name="inputPC" placeholder="CP" required>
+                <div class="form-group col-sm-4 col-sm-offset-1">
+                    <div class="col-sm-11">
+                        <div class="col-sm-1" style="color:#F62">*</div>
+                        <input type="text" class="form-control" id="inputPC" name="inputPC" placeholder="CP" required>
+                    </div>
                 </div>
-                <div class="form-group col-sm-6"><span style="color:#F62">*</span>
-                    <input type="text" class="form-control" id="inputCity" name="inputCity" placeholder="Ville"
-                           required>
+                <div class="form-group col-sm-6">
+                    <div class="col-sm-1" style="color:#F62">*</div>
+                    <div class="col-sm-11">
+                        <input type="text" class="form-control" id="inputCity" name="inputCity" placeholder="Ville"
+                               required>
+                    </div>
                 </div>
-                <div class="form-group col-sm-10 col-sm-offset-1"><span style="color:#F62">*</span>
-                    <input type="tel" class="form-control" id="inputPhoneNumber" name="inputPhoneNumber"
-                           placeholder="Numéro de téléphone" required>
+                <div class="form-group col-sm-10 col-sm-offset-1">
+                    <div class="col-sm-1" style="color:#F62">*</div>
+                    <div class="col-sm-11">
+                        <input type="tel" class="form-control" id="inputPhoneNumber" name="inputPhoneNumber"
+                               placeholder="Numéro de téléphone" required>
+                    </div>
                 </div>
                 <div class="form-group col-sm-10 col-sm-offset-1">
                     <input type="date" id="inputBirthDate" name="inputBirthDate" class="form-control">
